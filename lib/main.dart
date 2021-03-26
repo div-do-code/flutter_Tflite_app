74894971e41e103bddd5fd188bb25bb556827105
Div <javatestnoti@gmail.com>
2021-03-26 20:54:09 +0530

    minimum SDK version is updated to 21

---
+++ lib/main.dart
@@ -0,0 +1,201 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image/image.dart' as img;
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+const String ssd = "SSD MobileNet";
+const String yolo = "Tiny YOLOv2";
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TfliteHome(),
+    );
+  }
+}
+
+class TfliteHome extends StatefulWidget {
+  @override
+  _TfliteHomeState createState() => _TfliteHomeState();
+}
+
+class _TfliteHomeState extends State<TfliteHome> {
+  String _model = ssd;
+  File _image;
+
+  double _imageWidth;
+  double _imageHeight;
+  bool _busy = false;
+
+  List _recognitions;
+
+  @override
+  void initState() {
+    super.initState();
+    _busy = true;
+
+    loadModel().then((val) {
+      setState(() {
+        _busy = false;
+      });
+    });
+  }
+
+  loadModel() async {
+    Tflite.close();
+    try {
+      String res;
+      if (_model == yolo) {
+        res = await Tflite.loadModel(
+          model: "lib/assets/ssd_mobilenet.tflite",
+          labels: "lib/assets/ssd_mobilenet.txt",
+        );
+      } else {
+        res = await Tflite.loadModel(
+          model: "lib/assets/ssd_mobilenet.tflite",
+          labels: "lib/assets/ssd_mobilenet.txt",
+        );
+      }
+      print(res);
+    }
+    // using instead of catch block
+    on PlatformException {
+      print("Failed to load the model");
+    }
+  }
+
+  selectFromImagePicker() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+    setState(() {
+      _busy = true;
+    });
+    predictImage(image);
+  }
+
+  predictImage(File image) async {
+    if (image == null) return;
+
+    if (_model == yolo) {
+      await yolov2Tiny(image);
+    } else {
+      await ssdMobileNet(image);
+    }
+
+    FileImage(image)
+        .resolve(ImageConfiguration())
+        .addListener((ImageStreamListener((ImageInfo info, bool _) {
+          setState(() {
+            _imageWidth = info.image.width.toDouble();
+            _imageHeight = info.image.height.toDouble();
+          });
+        })));
+
+    setState(() {
+      _image = image;
+      _busy = false;
+    });
+  }
+
+  yolov2Tiny(File image) async {
+    var recognitions = await Tflite.detectObjectOnImage(
+        path: image.path,
+        model: "YOLO",
+        threshold: 0.3,
+        imageMean: 0.0,
+        imageStd: 255.0,
+        numResultsPerClass: 1);
+
+    setState(() {
+      _recognitions = recognitions;
+    });
+  }
+
+  ssdMobileNet(File image) async {
+    var recognitions = await Tflite.detectObjectOnImage(
+        path: image.path, numResultsPerClass: 1);
+
+    setState(() {
+      _recognitions = recognitions;
+    });
+  }
+
+  List<Widget> renderBoxes(Size screen) {
+    if (_recognitions == null) return [];
+    if (_imageWidth == null || _imageHeight == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageWidth * screen.width;
+    Color purple = Color.fromRGBO(128,0,128, 1.0);
+    return _recognitions.map((e) {
+      return Positioned(
+        left: e["rect"]["x"] * factorX,
+        top: e["rect"]["y"] * factorY,
+        width: e["rect"]["w"] * factorX,
+        height: e["rect"]["h"] * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            border: Border.all(
+              color: purple,
+              width: 5,
+            ),
+          ),
+          child: Text(
+            "${e["detectedClass"]} ${(e["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = Colors.purple,
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    List<Widget> stackChildren = [];
+
+    stackChildren.add(Positioned(
+      top: 0.0,
+      left: 0.0,
+      width: size.width,
+      child: _image == null ? Text('No image selected.') : Image.file(_image),
+    ));
+
+    stackChildren.addAll(renderBoxes(size));
+
+    if (_busy) {
+      stackChildren.add(Center(
+        child: CircularProgressIndicator(),
+      ));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter+Tflite = AI Deep Learning App'),
+        backgroundColor: Colors.cyan,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.image),
+        tooltip: "Select a Image",
+        onPressed: selectFromImagePicker,
+      ),
+      body: Stack(
+        children: stackChildren,
+      ),
+    );
+  }
+}
